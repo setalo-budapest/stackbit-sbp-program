0f627133fd58a780b0398b6746ce3bf258dae5a8
Stackbit <projects@stackbit.com>
2019-09-29 19:09:30 +0000

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
+    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
+else
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d90fb1cafd28b001303903f 
+fi
+jekyll build
